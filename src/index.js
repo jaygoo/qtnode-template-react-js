ae521e004a0787eebf98ff0e7fc20e97c656c533
guoyujie <296963166@qq.com>
2019-12-27 14:06:35 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Component from './components/Component';
-React;
-Component;
 ReactDom.render(<Component />, document.getElementById('root'));
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,8 +1,7 @@
 import App from './App';
 import React from 'react';
 import ReactDom from 'react-dom';
-App;
-React;
+
 ReactDom.render(
     <App/>,
     document.getElementById('root')
